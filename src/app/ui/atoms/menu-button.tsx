c528d54cc284cf898d13e48dcc958c997aa9e37a
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-28 20:07:17 -0300

    feat: add menu-button atom

---
+++ src/app/ui/atoms/menu-button.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes } from 'react'
+import Image from 'next/image'
+
+import { cn } from '@/helpers/cn'
+
+export type MenuButtonProps = Omit<
+	ButtonHTMLAttributes<HTMLButtonElement>,
+	'children'
+>
+
+export function MenuButton(props: MenuButtonProps) {
+	const { className, ...rest } = props
+	return (
+		<button
+			type="button"
+			title="Menu"
+			className={cn(
+				className,
+				'cursor-pointer rounded-full p-2 transition-colors hover:bg-zinc-100 focus:bg-zinc-100'
+			)}
+			{...rest}
+		>
+			<Image
+				src="/assets/icons/dots-vertical.svg"
+				alt="Menu Icon"
+				width={20}
+				height={20}
+			/>
+		</button>
+	)
+}
