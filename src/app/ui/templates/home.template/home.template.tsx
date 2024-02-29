be44400ecc28260e3fb2121021890ef59d3c0104
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-29 14:45:57 -0300

    chore: center car-table of home template

--- src/app/ui/templates/home.template/home.template.tsx
+++ src/app/ui/templates/home.template/home.template.tsx
@@ -6,7 +6,7 @@ import { Cars } from './components/cars'
 export function HomeTemplate() {
 	return (
 		<div className="w-full">
-			<div className="hidden sm:block">
+			<div className="hidden w-full sm:flex sm:items-center sm:justify-center">
 				<CarTable items={cars} />
 			</div>
 			<div className="sm:hidden">
